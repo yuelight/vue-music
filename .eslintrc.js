3420aac2e977c171b9a7f1542c37177928897764
yuelight <vaeqfe@gmail.com>
2017-08-18 18:58:59 +0800

    switch tab

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,13 +19,19 @@ module.exports = {
   'rules': {
     // allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
+    // allow asynccle-await
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
 
 	'indent': ['error', 'tab'],
 
-	'no-tabs': 'off'
+	'no-tabs': 'off',
+
+	'eol-last': 0,
+
+	'space-before-function-paren': 0,
+
+	'semi': 0
   }
 }
