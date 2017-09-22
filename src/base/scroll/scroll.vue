3db164ee5255918eb115bf032d9b39fd85bb5b10
yuelight <vaeqfe@gmail.com>
2017-09-22 19:28:50 +0800

    singer component

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -20,6 +20,10 @@
 			data: {
 				type: Array,
 				default: null
+			},
+			listenScroll: {
+				type: Boolean,
+				default: false
 			}
 		},
 		mounted() {
@@ -37,6 +41,12 @@
 					probeType: this.probeType,
 					click: this.click
 				});
+
+				if (this.listenScroll) {
+					this.scroll.on('scroll', pos => {
+						this.$emit('scroll', pos);
+					});
+				}
 			},
 			enable() {
 				this.scroll && this.scroll.enable();
@@ -46,6 +56,12 @@
 			},
 			refresh() {
 				this.scroll && this.scroll.refresh();
+			},
+			scrollTo() {
+				this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments);
+			},
+			scrollToElement() {
+				this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments);
 			}
 		},
 		watch: {
