e10ff9fed5077d6fbe36c22aa4d5e43765486c42
yuelight <vaeqfe@gmail.com>
2017-10-23 16:22:25 +0800

    player component

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -97,6 +97,9 @@
 			scroll(pos) {
 				this.scrollY = pos.y;
 			},
+			refresh() {
+				this.$refs.listview.refresh();
+			},
 			_scrollTo(index) {
 				if (!index && index !== 0) {
 					return;
