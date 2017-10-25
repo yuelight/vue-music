8c0bbaa53de02daf2b34ace88c762c61d416f4da
yuelight <vaeqfe@gmail.com>
2017-10-25 16:04:47 +0800

    rank component

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -4,9 +4,9 @@ export default function jsonp (url, data, option) {
 	url += (url.indexOf('?') < 0 ? '?' : '&') + param(data);
 
 	return new Promise((resolve, reject) => {
-		originJSONP(url, option, (err, data) => {
+		originJSONP(url, option, (err, res) => {
 			if (!err) {
-				resolve(data);
+				resolve(res);
 			} else {
 				reject(err);
 			}
