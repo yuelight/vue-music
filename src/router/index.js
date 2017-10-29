c46a1bc7abd11abb984e3d17cc34941b9cfc7a79
yuelight <vaeqfe@gmail.com>
2017-10-29 17:12:14 +0800

    search component

--- src/router/index.js
+++ src/router/index.js
@@ -47,8 +47,14 @@ export default new Router({
 			]
 		},
 		{
-			path: 'search',
-			component: Search
+			path: '/search',
+			component: Search,
+			children: [
+				{
+					path: ':id',
+					component: SingerDetail
+				}
+			]
 		}
 	]
 })
