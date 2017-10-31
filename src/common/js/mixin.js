cdafa3ed4a1cfb3835e976f3b5a45e5d23ed8914
yuelight <vaeqfe@gmail.com>
2017-10-31 09:08:12 +0800

    playlist component

--- src/common/js/mixin.js
+++ src/common/js/mixin.js
@@ -1,4 +1,6 @@
-import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
+import { playMode } from 'common/js/config';
+import { shuffle } from 'common/js/util';
 
 export const playlistMixin = {
 	computed: {
@@ -23,3 +25,44 @@ export const playlistMixin = {
 		}
 	}
 };
+
+export const playerMixin = {
+	computed: {
+		iconMode() {
+			return this.mode === playMode.sequence ? 'icon-sequence'
+				: this.mode === playMode.loop ? 'icon-loop' : 'icon-random';
+		},
+		...mapGetters([
+			'sequenceList',
+			'currentSong',
+			'playlist',
+			'mode'
+		])
+	},
+	methods: {
+		changeMode() {
+			let list = null;
+			const mode = (this.mode + 1) % 3;
+			this.setPlayMode(mode);
+			if (mode === playMode.random) {
+				list = shuffle(this.sequenceList);
+			} else {
+				list = this.sequenceList;
+			}
+			this.resetCurrentIndex(list);
+			this.setPlayList(list);
+		},
+		resetCurrentIndex(list) {
+			let index = list.findIndex(item => {
+				return item.id === this.currentSong.id;
+			});
+			this.setCurrentIndex(index);
+		},
+		...mapMutations({
+			setPlayingState: 'SET_PLAYING_STATE',
+			setCurrentIndex: 'SET_CURRENT_INDEX',
+			setPlayMode: 'SET_PLAY_MODE',
+			setPlayList: 'SET_PLAYLIST'
+		})
+	}
+}
--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -84,11 +84,12 @@
 						<i @click.stop="togglePlaying" class="icon-mini" :class="miniIcon"></i>
 					</progress-circle>
 				</div>
-				<div class="control">
+				<div class="control" @click.stop="showPlaylist">
 					<i class="icon-playlist"></i>
 				</div>
 			</div>
 		</transition>
+		<playlist ref="playlist"></playlist>
 		<audio ref="audio" :src="currentSong.url" @canplay="ready" @error="error" @timeupdate="updateTime" @ended="end"></audio>
 	</div>
 </template>
@@ -100,14 +101,16 @@
 	import ProgressBar from 'base/progress-bar/progress-bar';
 	import ProgressCircle from 'base/progress-circle/progress-circle';
 	import { playMode } from 'common/js/config';
-	import { shuffle } from 'common/js/util';
 	import Lyric from 'lyric-parser';
 	import Scroll from 'base/scroll/scroll';
+	import Playlist from 'components/playlist/playlist';
+	import { playerMixin } from 'common/js/mixin';
 
 	const transform = prefixStyle('transform');
 	const transitionDuration = prefixStyle('transitionDuration');
 
 	export default {
+		mixins: [playerMixin],
 		data() {
 			return {
 				songReady: false,
@@ -126,10 +129,6 @@
 			playIcon() {
 				return this.playing ? 'icon-pause' : 'icon-play';
 			},
-			iconMode() {
-				return this.mode === playMode.sequence ? 'icon-sequence'
-					: this.mode === playMode.loop ? 'icon-loop' : 'icon-random';
-			},
 			miniIcon() {
 				return this.playing ? 'icon-pause-mini' : 'icon-play-mini';
 			},
@@ -141,12 +140,8 @@
 			},
 			...mapGetters([
 				'fullScreen',
-				'playlist',
-				'currentSong',
 				'playing',
-				'currentIndex',
-				'mode',
-				'sequenceList'
+				'currentIndex'
 			])
 		},
 		created() {
@@ -283,24 +278,6 @@
 					this.currentLyric.seek(currentTime * 1000);
 				}
 			},
-			changeMode() {
-				let list = null;
-				const mode = (this.mode + 1) % 3;
-				this.setPlayMode(mode);
-				if (mode === playMode.random) {
-					list = shuffle(this.sequenceList);
-				} else {
-					list = this.sequenceList;
-				}
-				this.resetCurrentIndex(list);
-				this.setPlayList(list);
-			},
-			resetCurrentIndex(list) {
-				let index = list.findIndex(item => {
-					return item.id === this.currentSong.id;
-				});
-				this.setCurrentIndex(index);
-			},
 			getLyric() {
 				this.currentSong.getLyric().then(lyric => {
 					this.currentLyric = new Lyric(lyric, this.handleLyric);
@@ -322,6 +299,9 @@
 					this.$refs.lyricList.scrollTo(0, 0, 1000);
 				}
 				this.playingLyric = txt;
+			},
+			showPlaylist() {
+				this.$refs.playlist.show();
 			},
 			middleTouchStart(e) {
 				this.touch.initiated = true;
@@ -396,15 +376,14 @@
 				return { x, y, scale };
 			},
 			...mapMutations({
-				setFullScreen: 'SET_FULL_SCREEN',
-				setPlayingState: 'SET_PLAYING_STATE',
-				setCurrentIndex: 'SET_CURRENT_INDEX',
-				setPlayMode: 'SET_PLAY_MODE',
-				setPlayList: 'SET_PLAYLIST'
+				setFullScreen: 'SET_FULL_SCREEN'
 			})
 		},
 		watch: {
 			currentSong(newSong, oldSong) {
+				if (!newSong.id) {
+					return;
+				}
 				if (newSong.id === oldSong.id) {
 					return;
 				}
@@ -426,7 +405,8 @@
 		components: {
 			ProgressBar,
 			ProgressCircle,
-			Scroll
+			Scroll,
+			Playlist
 		}
 	}
 </script>
